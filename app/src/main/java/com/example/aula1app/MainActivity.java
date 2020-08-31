e22bbfc6cd244050bb3ca6bce2c947b17560b297
the0wl <kelvin.seibt@gmail.com>
2020-08-31 20:52:26 -0300

    Second commit

--- app/src/main/java/com/example/aula1app/MainActivity.java
+++ app/src/main/java/com/example/aula1app/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "MAIN_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +22,62 @@ public class MainActivity extends AppCompatActivity {
         Button btVoltar = (Button) findViewById(R.id.btVoltar);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(SecondActivity.NAME);
+        float degree = intent.getFloatExtra("DEGREE", 0);
+        tvNome.setText(String.valueOf(CelsiusToFahreinheit(degree)) + "º F");
 
-        tvNome.setText("Olá " + name + ", seja bem-vindo!");
+        Log.i(TAG, "Realizou o OnCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "Realizou o OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "Realizou o OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "Realizou o OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "Realizou o OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "Realizou o OnDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i(TAG, "Realizou o OnRestart");
+    }
+
+    public Float CelsiusToFahreinheit(float Celsius) {
+        return ((Celsius * 9) / 5) + 32;
     }
 
     public void trocaTela(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/example/aula1app/SecondActivity.java
+++ app/src/main/java/com/example/aula1app/SecondActivity.java
@@ -4,29 +4,72 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
-    public static final String NAME = "KELVIN";
-
     EditText etNome;
+    String TAG = "SECOND_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        etNome = (EditText) findViewById(R.id.etNome);
+        etNome = (EditText) findViewById(R.id.etDegree);
         Button btNext = (Button) findViewById(R.id.btNext);
 
+        Log.i(TAG, "Realizou o OnCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "Realizou o OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "Realizou o OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "Realizou o OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "Realizou o OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "Realizou o OnDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i(TAG, "Realizou o OnRestart");
     }
 
     public void trocaTela(View view) {
         Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra(NAME, etNome.getText().toString());
+        intent.putExtra("DEGREE", Float.parseFloat(etNome.getText().toString()));
         startActivity(intent);
     }
 
